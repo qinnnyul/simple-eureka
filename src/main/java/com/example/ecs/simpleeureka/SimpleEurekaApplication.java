4cb6e49ed9217ed425d37ea25021e98407ea71f2
Yulin Qin <yulin.qin.ext@gmail.com>
2018-12-23 17:15:56 +0800

    init the project

---
+++ src/main/java/com/example/ecs/simpleeureka/SimpleEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.example.ecs.simpleeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleEurekaApplication.class, args);
+	}
+
+}
+
